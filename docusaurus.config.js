ca434bcc5682bf2deb880e0001da43dbfb910f07
Muhammad Fakhri Imaduddin <fakhri.imaduddin@ruangguru.com>
2022-07-27 21:48:37 +0700

    feat: remove removed route

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -74,15 +73,6 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
           {
             title: 'Community',
             items: [
@@ -103,10 +93,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
